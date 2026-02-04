2258d6f6e0c002a6e44cef4ece01d72ba4047c88
Erik Soehnel <erik.soehnel@gmail.com>
2026-02-04 10:58:36 +0100

    drop papaparse dependency
    Parsed the csv data with python3 standard library and checked it into the
    repo instead, see apps/web/src/data.

--- apps/web/src/store.ts
+++ apps/web/src/store.ts
@@ -6,7 +6,6 @@ import {
   runMainStreetQuery,
   runAdminUnitQuery,
 } from "./queries";
-import { parseCsvAdminUnitsSaxony } from "./AdminUnitAddressSaxony";
 import type {
   LatLng,
   QueryResult,
@@ -21,7 +20,6 @@ interface Store {
 
   // Gemeinde oder Stadt-Schlüssel at query coordinate
   adminUnitAtCoord: NamedObjectWithPosition | null;
-  setAdminUnitAtCoord(adminUnitAtCoord: NamedObjectWithPosition): void;
 
   // main streets near query coordinate
   mainStreetsAtCoord: NamedObjectWithPosition[] | null;
@@ -44,53 +42,34 @@ export const useStore = create<Store>()(
       set((state) => ({ ...state, queryCoord }));
     },
 
-    adminUnitsSaxony: null,
-    setAdminUnitsSaxony: (adminUnitsSaxony) =>
-      set((state) => ({ ...state, adminUnitsSaxony: adminUnitsSaxony })),
-
     mainStreetsAtCoord: null,
-    setmainStreetAtCoord: (mainStreetAtCoord) =>
-      set((state) => ({ ...state, mainStreetsAtCoord: mainStreetAtCoord })),
-    adminUnitAtCoord: null,
-    setAdminUnitAtCoord: (adminUnitAtCoord) =>
-      set((state) => ({ ...state, adminUnitAtCoord: adminUnitAtCoord })),
     queryResult: null,
     setQueryResult: (queryResult) =>
       set((state) => ({ ...state, queryResult })),
 
     fetchQuery: async () => {
-      const { queryCoord, adminUnitsSaxony } = get();
+      const { queryCoord } = get();
 
-      let parsedAdminUnitsSaxony = adminUnitsSaxony;
-      if (!adminUnitsSaxony) {
-        try {
-          parsedAdminUnitsSaxony = await parseCsvAdminUnitsSaxony();
-          console.log(parsedAdminUnitsSaxony);
-        } catch (error) {
-          console.error(
-            "Fehler beim Laden der sächsischen Gemeinde-Daten:",
-            error,
-          );
-        }
+      if (!queryCoord) {
+        set((state) => ({
+          ...state,
+          mainStreetsAtCoord: null,
+        }));
+
+        return;
       }
-      //TODO : Do a get on the adminUnitsSaxony  if it is null, then call papa parse and set the result via
-      //setADminUnitsAtCoord()
 
-      // Debug alert(queryCoord?.lat.toString() + " " + queryCoord?.lng.toString());
+      let resultAdminUnitAtCoord: NamedObjectWithPosition | null = null;
+      let resultMainStreetsAtCoord: NamedObjectWithPosition[] = null;
+      // let resultSchools: NamedObjectWithPosition[];
+      // resultSchools = [];
 
-      let resultAdminUnitAtCoord: NamedObjectWithPosition | null;
-      resultAdminUnitAtCoord = null;
-
-      let resultMainStreetsAtCoord: NamedObjectWithPosition[];
-      resultMainStreetsAtCoord = [];
-      let resultSchools: NamedObjectWithPosition[];
-      resultSchools = [];
       //TODO Refactoring: Run queries in parallel and update Map / Result lists whenever one of the queries terminates
       if (queryCoord) {
-        resultAdminUnitAtCoord = (await runAdminUnitQuery(queryCoord)) || null;
-        console.log("Gefundener Kreis:", resultAdminUnitAtCoord);
-        resultMainStreetsAtCoord = await runMainStreetQuery(queryCoord);
-        console.log("Gefundene Straßen:", resultMainStreetsAtCoord);
+        // resultAdminUnitAtCoord = (await runAdminUnitQuery(queryCoord)) || null;
+        // console.log("Gefundener Kreis:", resultAdminUnitAtCoord);
+        // resultMainStreetsAtCoord = await runMainStreetQuery(queryCoord);
+        // console.log("Gefundene Straßen:", resultMainStreetsAtCoord);
 
         // In overpass distances are in meter, but the School server takes distances in km.
         resultSchools = await runSchoolQuery(queryCoord, 0.3);
@@ -100,9 +79,7 @@ export const useStore = create<Store>()(
       set((state) => ({
         ...state,
         mainStreetsAtCoord: resultMainStreetsAtCoord,
-        adminUnitAtCoord: resultAdminUnitAtCoord,
         queryResult: { pointsOfSchools: resultSchools },
-        adminUnitsSaxony: parsedAdminUnitsSaxony,
       }));
     },
   })),
