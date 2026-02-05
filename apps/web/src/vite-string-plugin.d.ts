c19523d307b34ac85e260276a03aa89faaf03402
Erik Soehnel <erik.soehnel@gmail.com>
2026-02-05 11:57:33 +0100

    remove schulserver and csv related vite settings
    not needed any more as we got the json export data at hand

--- apps/web/src/vite-string-plugin.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "vite-string-plugin" {
-  export interface ViteStringPluginOpts {
-    // You can define any necessary options here
-  }
-  export default function stringPlugin(opts?: ViteStringPluginOpts): any;
-}
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,23 +1,11 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
-import { stringPlugin } from "vite-string-plugin";
-//import parseFilePlugin from "./vite-plugin-parse-file";
-// https://vite.dev/config/
+
 export default defineConfig({
   // base path for gh-pages
   base: "projekt30/",
-  plugins: [stringPlugin(), preact()],
+  plugins: [preact()],
   server: {
-    proxy: {
-      "/sachsen-schul-api": {
-        target: "https://schuldatenbank.sachsen.de/api",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/sachsen-schul-api/, ""),
-        headers: {
-          "Access-Control-Allow-origin": "*",
-        },
-      },
-    },
     allowedHosts: [".ngrok-free.app"],
   },
 });
